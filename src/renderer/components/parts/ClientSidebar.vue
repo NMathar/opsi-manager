be5f271418df39a01bdebcaa1c10856f4f199200
NMathar <nicolai.mathar@ima.rwth-aachen.de>
2019-02-27 14:18:08 +0100

    add software table

--- src/renderer/components/parts/ClientSidebar.vue
+++ src/renderer/components/parts/ClientSidebar.vue
@@ -1,39 +1,39 @@
 <template>
-    <div>
-        <div class="sticky-top bg-white">
-            <b-nav fill tabs>
-                <b-nav-item active>
-                    <font-awesome-icon icon="desktop"/>
-                </b-nav-item>
-                <b-nav-item>
-                    <font-awesome-icon icon="th-list"/>
-                </b-nav-item>
-            </b-nav>
+  <div>
+    <div class="sticky-top bg-white">
+      <b-nav fill tabs>
+        <b-nav-item active>
+          <font-awesome-icon icon="desktop"/>
+        </b-nav-item>
+        <b-nav-item>
+          <font-awesome-icon icon="th-list"/>
+        </b-nav-item>
+      </b-nav>
 
-            <b-form inline class="p-2">
-                <label class="sr-only" for="clientSearch">Search</label>
-                <b-input-group class="mb-2 mr-sm-2 mb-sm-0">
-                    <b-input id="clientSearch" placeholder="Search"/>
-                </b-input-group>
+      <b-form inline class="p-2">
+        <label class="sr-only" for="clientSearch">Search</label>
+        <b-input-group class="mb-2 mr-sm-2 mb-sm-0">
+          <b-input id="clientSearch" placeholder="Search"/>
+        </b-input-group>
 
-                <b-button variant="primary">
-                    <font-awesome-icon icon="search"/>
-                </b-button>
-            </b-form>
-        </div>
-        <b-nav vertical v-if="!groupNav">
-            <b-nav-item v-for="client in clients" :key="client.id"
-                        :to="{ name: 'client-page', params: { id: client.id }}">{{client.id}}
-            </b-nav-item>
-        </b-nav>
-        <b-nav vertical v-if="groupNav">
-            <b-nav-item-dropdown v-for="(group, index) in groups" :key="index" :text="index">
-                <b-dropdown-item v-for="client in group" :key="client.clientId">
-                    {{client.clientId}}
-                </b-dropdown-item>
-            </b-nav-item-dropdown>
-        </b-nav>
+        <b-button variant="primary">
+          <font-awesome-icon icon="search"/>
+        </b-button>
+      </b-form>
     </div>
+    <b-nav vertical v-if="!groupNav">
+      <b-nav-item v-for="client in clients" :key="client.id"
+                  :to="{ name: 'client-page', params: { id: client.id }}">{{client.id}}
+      </b-nav-item>
+    </b-nav>
+    <b-nav vertical v-if="groupNav">
+      <b-nav-item-dropdown v-for="(group, index) in groups" :key="index" :text="index">
+        <b-dropdown-item v-for="client in group" :key="client.clientId">
+          {{client.clientId}}
+        </b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-nav>
+  </div>
 </template>
 
 <script>
@@ -46,7 +46,7 @@
     name: 'ClientSidebar',
     data () {
       return {
-        groupNav: true,
+        groupNav: false,
         clients: [],
         groups: []
       }
@@ -57,31 +57,20 @@
       }
     },
     methods: {
-      async getGroups () {
-        const {data} = await this.api.getAllHostGroupsWithClients()
-        await this.$nextTick()
-        this.groups = data
-        console.log(this.groups)
-      }
-    },
-    watch: {
-      groups: function (newGroups, oldGroups) {
-        console.log(oldGroups)
-        console.log(newGroups)
-        // console.log(Object.keys(newGroups).length)
+      sortBy (array, p) {
+        return array.slice(0).sort(function (a, b) {
+          return (a[p] > b[p]) ? 1 : (a[p] < b[p]) ? -1 : 0
+        })
       }
     },
     mounted () {
-      let self = this
       this.api.getAllClients().then((res) => {
         // console.log(res.data)
-        this.clients = res.data
+        this.clients = this.sortBy(res.data, 'id')
       })
-      this.$nextTick(function () {
-        self.api.getAllHostGroupsWithClients().then((res) => {
-          // console.log(res.data)
-          self.groups = res.data
-        })
+      this.api.getAllHostGroupsWithClients().then((res) => {
+        // console.log(res.data)
+        self.groups = res.data
       })
     }
   }
--- src/renderer/components/tabs/Software.vue
+++ src/renderer/components/tabs/Software.vue
@@ -1,14 +1,55 @@
 <template>
-    <div class="software">
+  <div class="software">
+    <b-row>
+      <b-col md="5" class="my-4">
         <h3>Software</h3>
-        {{softwaredata}}
-    </div>
+      </b-col>
+      <b-col offset-md="2" md="5" class="my-4">
+        <b-form-group class="mb-0">
+          <b-input-group>
+            <b-form-input v-model="filter" placeholder="Type to Search" />
+            <b-input-group-append>
+              <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+    </b-row>
+    <b-table striped hover :filter="filter" :items="softwaredata" :fields="fields"/>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'Software',
-    props: ['softwaredata']
+    props: ['softwaredata'],
+    data () {
+      return {
+        filter: null,
+        fields: {
+          productId: {
+            label: 'Product',
+            sortable: true
+          },
+          productVersion: {
+            label: 'Version',
+            sortable: false
+          },
+          installationStatus: {
+            label: 'State',
+            sortable: false
+          },
+          actionResult: {
+            label: 'Action Result',
+            sortable: false
+          },
+          modificationTime: {
+            label: 'last change',
+            sortable: true
+          }
+        }
+      }
+    }
   }
 </script>
 
